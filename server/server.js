c6bc45f0b620c731027c874521fa5552f3581118
liuyun19 <liuyun19@jd.com>
2018-06-27 18:44:12 +0800

    react 基础回顾

--- server/server.js
+++ server/server.js
@@ -1,6 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
+//1. 连接mongo 并且使用interview这个集合
+const DB_URL= 'mongodb://localhost:27017/interview';
+mongoose.connect(DB_URL);
+mongoose.connection.on('connected', function() {
+    console.log('mongo connect success')
+});
+
+// 类似于mysql的表 mongo里有文档、字段的概念
+
+const User = mongoose.model('user', new mongoose.Schema({
+    user: {type: String, require: true},
+    age: {type: Number, require: true}
+}));
+// create 新增数据
+/*User.create({
+    name: 'Linda',
+    age: 19
+}, function(err, doc) {
+    if(!err) {
+        console.log(doc);
+    } else {
+        console.log(err);
+    }
+});*/
+
 
 // 新建app
+// User.remove({age: 19}, function(err, doc) {
+//     console.log('--->'+ JSON.stringify(doc));
+// });
+// User.update({'age': 19}, {'$set': {age: 26}}, function(err, doc) {
+//     console.log('update--->' + JSON.stringify(doc));
+// });
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -8,8 +41,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/data', (req, res) => {
-    res.json({name: 'imooc app', type: 'IT'});
-})
+    // 查找数据
+    User.find({}, function(err, doc){
+        return res.json(doc);
+    });
+    // User.findOne({age:19}, function(err, doc){
+    //     return res.json(doc);
+    // });
+    // res.json({name: 'imooc app', type: 'IT'});
+});
 
 app.listen(9093, function() {
     console.log('Node app start at port 9093');
--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {createStore} from 'redux';
+import React from 'react';
+import {Button, List} from 'antd-mobile';
 
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React111</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        const boss = '阮经天';
+        return (
+            <div>
+                <h2>hello {boss} </h2>
+                <一营 老大='张大大'></一营>
+                <骑乒连 老大="陈大大"></骑乒连>
+            </div>
+        )
+    }
 }
 
-export default App;
+function 骑乒连(props) {
+    return <h2>骑兵连连长{props.老大}, 冲啊！</h2>
+}
+
+class 一营 exends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            soldiers: ['虎子', '柱子', '王根生']
+        }
+        // this找不到解决方案一：
+        // this.addSoldier = this.addSoldier.bind(this);
+    }
+    componentWillMount() {
+        console.log('组件马上就要加载了')
+    }
+    componentDidMount() {
+        console.log('组件加载完毕')
+    }
+    addSoldier() {
+        console.log('hello add soldier');
+        this.setState({
+            soldiers: [...this.state.soldiers, '新兵蛋子' + Math.random()]
+        });
+    }
+    render() {
+        console.log('组件正在加载')
+        return (
+            <div>
+                <h2>一营营长, 咱们老大是{this.props.老大}</h2>
+                {/*this找不到解决方案一：*/}
+                <Button type='primary' onClick={() => this.addSoldier()}>新兵入伍</Button>
+                <List renderHeader={() => '士兵列表'}>
+                    {
+                        this.state.soldiers.map(v => {
+                            return (
+                                <List.Item key={v}>{v}</List.Item>
+                            )
+                        })
+                    }
+                </List>
+                <ul>
+                    {this.state.soldiers.map(v => {
+                        return <li key={v}>{v}</li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default  App;
