07aaed415f17d94b9e0076cf50620d61ae70dfd4
liuyun19 <liuyun19@jd.com>
2018-06-28 14:30:52 +0800

    react-redux使用

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import React from 'react';
-// import {add} from './index.redux';
+import {addGun, removeGun, addGunAsync} from './index.redux';
+import {connect} from 'react-redux';
+
+/*
+ * Connect负责从外部获取组件需要的参数
+ * Connect可以用装饰器的方式来写
+ *
+ *
+ * connect有两个参数，传入两个参数后，这样react-redux会自动把这些参数放到props里面去
+ */
+// App需要的属性
+// const mapStatetoProps = (state) => {
+//     return {num: state};
+// }
+// App需要的方法的集合
+// const actionCreators = {addGun, removeGun, addGunAsync};
+
+// // connect包裹App
+// App = connect(mapStatetoProps, actionCreators)(App);
+
+//装饰器模式
+@connect(
+    // 你要state什么属性放到props里面
+    state => ({num: state}),
+    // 你要的方法，放到props里面，自动dispatch
+    {addGun, removeGun, addGunAsync}
+)
+
 
 class App extends React.Component {
    /* constructor(props) {
@@ -7,20 +34,23 @@ class App extends React.Component {
     }*/
 
     render() {
-        const store = this.props.store;
-        const num = store.getState();
-        const addGun = this.props.addGun;
-        const addGunAsync = this.props.addGunAsync;
-        const removeGun = this.props.removeGun;
+        // const store = this.props.store;
+        // const num = store.getState();
         return (
            <div>
-               <h1>现在有机枪{num}</h1>
-               <button onClick={() => store.dispatch(addGun())}>申请机枪</button>
-               <button onClick={() => store.dispatch(removeGun())}>上交机枪</button>
-               <button onClick={() => store.dispatch(addGunAsync())}>拖两天再上交机枪</button>
+               <h1>现在有机枪{this.props.num}</h1>
+               {/*<button onClick={() => store.dispatch(addGun())}>申请机枪</button>*/}
+               {/*<button onClick={() => store.dispatch(removeGun())}>上交机枪</button>*/}
+               {/*<button onClick={() => store.dispatch(addGunAsync())}>拖两天再上交机枪</button>*/}
+
+               {/*num addGun removeGun addGunAsync都是connect给的，不需要手动dispatch，自动有了dispatch了*/}
+               <button onClick={this.props.addGun}>申请机枪</button>
+               <button onClick={this.props.removeGun}>上交机枪</button>
+               <button onClick={this.props.addGunAsync}>拖两天再上交机枪</button>
            </div>
         )
     }
 }
+
 
 export default  App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore, applyMiddleware, compose} from 'redux'; // 使用applyMiddleware开启thunk中间件
 import thunk from 'redux-thunk';
-import {counter, addGun, removeGun, addGunAsync} from './index.redux';
+/**
+ * Provider组件在应用最外层，传入store即可，其他参数不用再传入了，只用一次
+ * Connect负责从外部获取组件需要的参数
+ * Connect可以用装饰器的方式来写
+ */
+import {Provider} from 'react-redux';
+// import {counter, addGun, removeGun, addGunAsync} from './index.redux';
+import {counter} from './index.redux';
 
 const store = createStore(counter, compose(
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f=> f
     ));
 
-function render() {
+// 使用react-redux就可以不用subscribe
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
+
+
+/*function render() {
     ReactDOM.render(
         <App store={store}
              addGun={addGun}
@@ -22,5 +37,5 @@ function render() {
 render();
 
 // 状态改变之后手动执行一下render
-store.subscribe(render);
+store.subscribe(render);*/
 
